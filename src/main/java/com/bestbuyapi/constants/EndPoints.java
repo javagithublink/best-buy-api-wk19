256d05058c5db775c1fe478cb9389992e602010e
javagithublink <javagithublink@gmail.com>
2022-01-09 19:59:16 +0000

    BestBuy API Week 19

---
+++ src/main/java/com/bestbuyapi/constants/EndPoints.java
@@ -0,0 +1,9 @@
+package com.bestbuyapi.constants;
+
+public class EndPoints {
+
+    public static final String GET_STORE_BY_ID = "/{storeID}";
+    public static final String CHANGE_STORE_INFO = "/{storeID}";
+    public static final String DELETE_STORE="/{storeID}";
+
+}
